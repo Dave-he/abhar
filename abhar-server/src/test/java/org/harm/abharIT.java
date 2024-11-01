71fdbae7e4feaba20e961770a8563a24573c64d0
heyongxian <heyongxian@kuaishou.com>
2024-11-01 17:59:22 +0800

    init

---
+++ abhar-server/src/test/java/org/harm/abharIT.java
@@ -0,0 +1,8 @@
+package org.harm;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+class abharIT extends abharTest {
+    // Execute the same tests but in packaged mode.
+}
